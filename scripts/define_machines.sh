92f539b27c93dfec7dbe3cd658a1cdda3e8b1394
Marian Gasparovic <marian.gasparovic@canonical.com>
2018-09-05 11:45:28 +0200

    added define_machines.sh

---
+++ scripts/define_machines.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+define() {
+# $1 name
+# $2 id
+# $3 memory
+# $4 - $8 unique MAC
+
+set -x
+
+echo $1 $2 $3 $4 $5 $6 $7 $8
+
+virsh undefine ${1}${2}
+
+sleep 3
+
+CPUOPTS="--cpu host"
+GRAPHICS="--graphics vnc --video=cirrus"
+CONTROLLER="--controller scsi,model=virtio-scsi,index=0"
+DISKOPTS="format=qcow2,bus=scsi,cache=writeback"
+export CPUOPTS GRAPHICS CONTROLLER DISKOPTS
+
+qemu-img create -f qcow2 ${1}${2}d1.qcow2 40G
+qemu-img create -f qcow2 ${1}${2}d2.qcow2 20G
+qemu-img create -f qcow2 ${1}${2}d3.qcow2 20G
+qemu-img create -f qcow2 ${1}${2}d4.qcow2 20G
+qemu-img create -f qcow2 ${1}${2}d5.qcow2 20G
+qemu-img create -f qcow2 ${1}${2}d6.qcow2 20G
+
+virt-install --noautoconsole --print-xml --boot network,hd,menu=on \
+$GRAPHICS $CONTROLLER --name ${1}${2} --ram $3 --vcpus 2 $CPUOPTS \
+--disk path=${1}${2}d1.qcow2,size=40,$DISKOPTS \
+--disk path=${1}${2}d2.qcow2,size=20,$DISKOPTS \
+--disk path=${1}${2}d3.qcow2,size=20,$DISKOPTS \
+--disk path=${1}${2}d4.qcow2,size=20,$DISKOPTS \
+--disk path=${1}${2}d5.qcow2,size=20,$DISKOPTS \
+--disk path=${1}${2}d6.qcow2,size=20,$DISKOPTS \
+--network=bridge=maasbr0,mac=${4}:${5}:${6}:${7}:${8}:1${2},model=virtio \
+--network=bridge=maasbr1,mac=${4}:${5}:${6}:${7}:${8}:2${2},model=virtio \
+> ${1}${2}.xml
+
+virsh define ${1}${2}.xml
+}
+
+for i in $(seq 1 9); do
+  define swift ${i} 8192 $(date +"%y %m %H %M %S")
+  define ceph ${i} 8192 $(date +"%y %m %H %M %S")
+done
